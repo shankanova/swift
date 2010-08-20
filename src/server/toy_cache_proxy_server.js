15c91767888569e5464d0b94f96432dce6316cfb
Shirshanka Das <shirshanka@gmail.com>
2010-08-20 16:13:08 -0700

    Basic proxy server works

--- src/server/toy_cache_proxy_server.js
+++ src/server/toy_cache_proxy_server.js
@@ -4,38 +4,82 @@ var sys = require("sys"),
     http = require("http"),
     url = require("url"),
     path = require("path"),
-    fs = require("fs");
-    circular_buffer = require("../util/circular_buffer");
+    fs = require("fs"),
+	CircularCacheFile = require("../util/circularCacheFile"),
+	CacheIndex = require('../util/CacheIndex');
 
 
-circularBuffer = new circular_buffer.CircularBuffer;
+process.argv.forEach(function (val, index, array) {
+  console.log(index + ': ' + val);
+});
 
-http.createServer(function(request, response) {
-    var uri = url.parse(request.url).pathname;
-    
-    var filename = path.join(process.cwd(), uri);
-    path.exists(filename, function(exists) {
-    	if(!exists) {
-    		response.sendHeader(404, {"Content-Type": "text/plain"});
-    		response.write("404 Not Found\n");
-    		response.close();
-    		return;
-    	}
 
-    	fs.readFile(filename, "binary", function(err, file) {
-    		if(err) {
-    			response.sendHeader(500, {"Content-Type": "text/plain"});
-    			response.write(err + "\n");
-    			response.close();
-    			return;
-    		}
+var cacheFileName = process.argv[2];
+var cacheFileSize = process.argv[3];
+var truncate = process.argv[4];
+var myCacheFile = new CircularCacheFile(cacheFileName, cacheFileSize, truncate);
+var maxCacheIndexSize = 1000000;
+var myCacheIndex = new CacheIndex("/tmp",maxCacheIndexSize);
+ 
+var server = http.createServer(function(request, response) {
+	var operation = url.parse(request.url).pathname;
+    switch (operation) {
+	case '/':
+		showForm(request, response);
+		break;
+	case '/get':
+		getResource(request, response);
+		break;
+	case '/put':
+		putResource(request, response);
+		break;
+	default:
+		show404(request, response, "Sorry no such operation: " + operation);
+		break;
+	}
+});
 
+server.listen(8080);
+
+
+function showForm(req, res) {
+  res.sendHeader(200, {'Content-Type': 'text/html'});
+  res.write(
+    '<form action="/put" method="post" enctype="multipart/form-data">'+
+    '<input type="text" name="json-body">'+
+    '<input type="submit" value="Put">'+
+    '</form>'
+  );
+  res.close();
+}
+
+function show404(request, response, errorText) {
+	response.sendHeader(404);
+	response.write(errorText, "UTF-8");
+	response.close();
+}
+
+function getResource(request, response) {
+	var uriParsed = url.parse(request.url, true);
+	if (uriParsed.hasOwnProperty('query') && uriParsed.query.hasOwnProperty('id')) {
+			var idRequested = uriParsed.query.id;
     		response.sendHeader(200);
-    		response.write(file, "binary");
-    		response.close();
-    	});
-    });
-}).listen(8080);
+    		response.write("get resource" + idRequested, "UTF-8");
+			response.close();
+	  }
+	else 
+	 {
+		show404(request, response, "get request was malformed. Usage: /get?id=resource_id");
+	}
+	
+}
+
+
+function putResource(request, response) {
+    response.sendHeader(200);
+    response.write(textResponse, "UTF-8");
+    response.close();
+}
 
 sys.puts("Server running at http://localhost:8080/");
 
