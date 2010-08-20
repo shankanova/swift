602bea7cb770bcaf797b6467f0282d0f7f6080a2
Swee Lim <slim@slim-mn.local>
2010-08-20 13:57:55 -0700

    testing

--- src/server/main.js
+++ src/server/main.js
@@ -1,16 +1,27 @@
 #!/usr/local/bin/node
 
 var sys = require("sys"),  
+    crypto = require("crypto");
     CircularCacheFile = require('../util/circularCacheFile'),
     CacheIndex = require('../util/CacheIndex');
 
 
-myCacheFile = new CircularCacheFile("/tmp/cache");
-myCacheFile.put(0,56325623,new Buffer('metadata'),
-    new Buffer('body'),sys.puts);
-myCacheFile.get(0,56325623,sys.puts);
-myCacheFile.getMetadata(56325623,'',sys.puts);
+function testCircularCacheFile(fileName, maxSize, numEntries, entrySize)
+{
+//    var myCacheFile = new CircularCacheFile(fileName, maxSize, false);
+    var offset = 0;
+    var buffer = new Buffer(entrySize);
+    for (var i = 0; i < numEntries; i = i + 1) {
+        var hash = crypto.createHash('md5');
+        var sig = hash.update(i).digest('binary');
+sys.puts(sig.length);
+sys.puts(sig);
+//        myCacheFile.put(offset, sig, buffer, sys.puts);
+//myCacheFile.get(0,sig,sys.puts);
+//myCacheFile.getMetadata(sig,'',sys.puts);
+    }
+}
 
 
-myCacheIndex = new CacheIndex('/tmp/cacheIndex',10000);
+testCircularCacheFile('/tmp/cacheIndex',10000, 100, 200);
 
