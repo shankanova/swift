10eed3fffa2151be9e39b7cde9b9cf5ea8c7d1e1
Swee Lim <slim@slim-mn.local>
2010-08-21 09:17:36 -0700

    ignore connection and keep-alive in header

--- src/util/CacheUtils.js
+++ src/util/CacheUtils.js
@@ -1,10 +1,24 @@
 var crypto = require("crypto"),
-	url = require("url")
+	url = require("url"),
+  fs = require("fs"),
+  sys = require("sys")
 	;
+
+eval(fs.readFileSync('../lib/json2.js'));
 
 function computeCacheKey(request) {
 	var hash = crypto.createHash('md5');
 	hash.update(request.url);
+  var cloneHeaders = {};
+  for (var i in request.headers) 
+  {
+    if (i != 'connection' && i != 'keep-alive') 
+    {
+      cloneHeaders[i] = request.headers[i];
+    }
+  }
+  var headerJSON = JSON.stringify(cloneHeaders);
+  hash.update(headerJSON);
 	var sig = hash.digest('binary');
 	return sig;
 }
